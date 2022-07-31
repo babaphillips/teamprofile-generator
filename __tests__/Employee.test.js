aa664985ece73192f5e53dff6da7806009e12796
babaphillips <amandavphillips@icloud.com>
2022-07-31 08:47:45 -0400

    half way thru employee tests

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -7,7 +7,26 @@ test('creates a new employee', () => {
 });
 
 test('can set name thru constructor arguments', () => {
-    const name = 'Amanda';
-    const employee = new Employee(name);
-    expect(employee.name).toBe(name);
+    const testName = 'Amanda';
+    const employee = new Employee(testName);
+    expect(employee.name).toBe(testName);
 })
+
+test('can set id thru constructor arguments', () => {
+    const testId = 200;
+    const employee = new Employee("Amanda", testId);
+    expect(employee.id).toBe(testId);
+})
+
+test('can set email thru constructor arguments', () => {
+    const testEmail = "amandavphillips@icloud.com";
+    const employee = new Employee("Julia", 12, testEmail);
+    expect(employee.email).toBe(testEmail);
+})
+
+test('can get name via getName() function', () => {
+    const testName = 'Amanda';
+    const employee = new Employee(testName);
+    expect(employee.getName()).toBe(testName);
+})
+
