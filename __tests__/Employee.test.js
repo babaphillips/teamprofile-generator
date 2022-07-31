ea97d8b83ce8358f8b7782fb0342dd5c2673b7ce
babaphillips <amandavphillips@icloud.com>
2022-07-31 09:21:24 -0400

    all employee tests passed

--- __tests__/Employee.test.js
+++ __tests__/Employee.test.js
@@ -16,17 +16,33 @@ test('can set id thru constructor arguments', () => {
     const testId = 200;
     const employee = new Employee("Amanda", testId);
     expect(employee.id).toBe(testId);
-})
+});
 
 test('can set email thru constructor arguments', () => {
     const testEmail = "amandavphillips@icloud.com";
     const employee = new Employee("Julia", 12, testEmail);
     expect(employee.email).toBe(testEmail);
-})
+});
 
 test('can get name via getName() function', () => {
     const testName = 'Amanda';
     const employee = new Employee(testName);
     expect(employee.getName()).toBe(testName);
-})
+});
+
+test('can get id via getId() function', () => {
+    const testId = 123;
+    const employee = new Employee("Amanda", testId);
+    expect(employee.getId()).toBe(testId);
+});
+
+test('can get email via getEmail() function', () => {
+    const testEmail = "amandavphillips@icloud.com";
+    const employee = new Employee("Amanda", 12, testEmail);
+});
+
+test('can get role via getRole() function', () => {
+    const testRole = "Employee";
+    const employee = new Employee("Jade", 17, "amandavphillips@icloud.com")
+});
 
